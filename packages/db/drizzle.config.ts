fa1cfcf7f615652a164597e2ce7ff2a2689e1364
Michael Faißt <michael@codenite.at>
2023-04-14 18:06:17 +0200

    added db scripts

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import type { Config } from "drizzle-kit";
 
 const config: Config = {
-    schema: "./src/db/schema.ts",
+    schema: "./src/schema.ts",
     out: "./migrations",
     connectionString: process.env.DB_URL
 };
